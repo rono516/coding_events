b1658e275c2677c9974a2225e9871c7fa866e22f
rono516 <ronocollins2000@gmail.com>
2023-02-18 01:04:26 +0300

    refactor code

--- resources/views/codechef.blade.php
+++ resources/views/codechef.blade.php
@@ -1,37 +1,29 @@
 @include('layouts.inc.topnav')
 
-<section class="bg-light p-5">
-  <div class="header_fixed table-responsive" id="no-more-tables">
-    <table class="table bg-white">
-     <thead class="bg-dark text-light">
-        <th>Contest Name</th>
-        <th>Contest Link</th>
-        <th>Start Time</th>
-        <th>End Time</th>
-        <th>Duration</th>
-        {{-- <th>Site</th> --}}
-        <th>In 24hrs?</th>
-        <th>Coding Status</th>
-
-       </thead>
-       <tbody>
-         @foreach ($codechef as $event)
-
-           <tr>
-              <td data-title="Contest Name" style="width: 250px;">{{ $event->name }}</td>
-              <td data-title="Contest Link" style="width: 200px;"> <a href="{{ $event->url }}">{{ $event->url }}</a></td>
-              <td data-title="Start Time">{{ $event->start_time }}</td>
-              <td data-title="End Time">{{ $event->end_time }}</td>
-              <td data-title="Duration">{{ $event->duration }}</td>
-              {{-- <td data-title="Site">{{ $event->site }}</td> --}}
-              <td data-title="In 24hrs?">{{ $event->in_24_hours }}</td>
-              <td data-title="Coding Status">{{ $event->status }}</td>
+<div class="py-3 mx-2">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Contest Name</th>
+                <th scope="col">Contest Link</th>
+                <th scope="col">Start Time</th>
+                <th scope="col">Duration</th>
+                <th scope="col">In 24hrs?</th>
+                <th scope="col">Coding Status</th>
             </tr>
-            
-           @endforeach
-
-         </tbody>
-      </table>
-
-   </div>
-</section>
+        </thead>
+        <tbody>
+            @foreach ($codechef as $event)
+                <tr>
+                    <td data-title="Contest Name">{{ $event->name }}</td>
+                    <td data-title="Contest Link"> <a target="_blank" href="{{ $event->url }}">{{ $event->url }}
+                        </a></td>
+                    <td data-title="Start Time">{{ date('d/M/Y H:i:s', strtotime($event->start_time)) }}</td>
+                    <td data-title="Duration">{{ gmdate('H:i:s', $event->duration) }}</td>
+                    <td data-title="In 24hrs?">{{ $event->in_24_hours }}</td>
+                    <td data-title="Coding Status">{{ $event->status }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
--- resources/views/layouts/inc/topnav.blade.php
+++ resources/views/layouts/inc/topnav.blade.php
@@ -1,56 +1,58 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-popRpmFF9JQgExhfw5tZT4I9/CI5e2QcuUZPOVXb1m7qUmeR2b50u+YFEYe1wgzy" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-popRpmFF9JQgExhfw5tZT4I9/CI5e2QcuUZPOVXb1m7qUmeR2b50u+YFEYe1wgzy" crossorigin="anonymous">
+</script>
 <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
- <link rel="stylesheet" href="https://poshui-components.netlify.app/css/main.css"/> 
-  
-  <nav style="margin-left: 2.5%; margin-right:2.5%;" class="navbar navbar-expand-sm navbar-dark bg-dark justify-content-centre">
+<link rel="stylesheet" href="https://poshui-components.netlify.app/css/main.css" />
 
-    <div class="container-fluid">
-      <figure>
-        <img class="img img-round margin-lr-1" style="border: 5px solid white;" height="170px" width="170px" src="{{ asset('img/CodeiT.png') }}" alt="">
-        <figcaption>By Rono</figcaption>
-      </figure>
+<style>
+    body {
+        overflow-x: hidden;
+    }
+</style>
 
-      <!-- <a href="{{ url('/') }}" class="navbar-brand" style=" color:blue;" >CodeiT by Rono</a> -->
-      <a href="{{ url('/') }}" class="navbar-brand" style=" color:blue; font-size: 30px;" >Coding Contests</a>
-      
-      
-      
-      
-      
-      <button type="button" data-toggle="collapse" data-target="#navbar-menu" class="navbar-toggler">
-         <span class="navbar-toggler-icon"></span> </button>
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
 
-    <div class="parent">
-      <div class="collapse navbar-collapse mr-4 center-me" id="navbar-menu">  
-        
+    <div class="container">
+        <a href="{{ url('/') }}" class="navbar-brand">Coding Contests<span style="color: blue">/Events</span></a>
+    </div>
+
+
+    <button type="button" data-toggle="collapse" data-target="#navbar-menu" class="navbar-toggler">
+        <span class="navbar-toggler-icon"></span> </button>
+
+    <div class="collapse navbar-collapse mr-4 center-me" id="navbar-menu">
+
         <ul class="navbar-nav mr-auto">
-          <li><a href="{{ url('/') }}" class="nav-link">All Contests</a></li>
-          <li><a href="{{ url('/hackerearth') }}" class="nav-link">Hacker Earth</a></li>
-          <li><a href="{{ url('/codeforces') }}" class="nav-link">CodeForces</a></li>
-       
-          <!-- <li><a href="{{ url('/topcoder') }}" class="nav-link">TopCoder</a></li>
-          <li><a href="{{ url('/atcoder') }}" class="nav-link">AtCoder</a></li>
-          <li><a href="{{ url('/codechef') }}" class="nav-link">CodeChef</a></li> -->
-          <li><a href="{{ url('/hackerrank') }}" class="nav-link">HackerRank</a></li>
-          <li><a href="{{ url('/kickstart') }}" class="nav-link">Kick Start</a></li>
-          <li><a href="{{ url('/leetcode') }}" class="nav-link">LeetCode</a></li>
-          <!-- <li><a href="{{ url('/sites') }}" class="nav-link">Contest Sites</a></li> -->
-          <li><a href="https://www.facebook.com/collins.sharks.3/">
-          <button class="btn btn-primary-solid btn-rounded"><i class="fa fa-facebook"  style="font-size:28px;"></i></button>
-          </a></li>
-          <li><a href="https://github.com/rono516/">
-           <button class="btn btn-primary-solid btn-rounded"><i class="fa fa-github" style="font-size:28px"></i></button>
-          </a></li>
-          <li><a href="https://mobile.twitter.com/Collins42Rono/">
-          <button class="btn btn-primary-solid btn-rounded"><i class="fa fa-twitter" style="font-size:28px"></i></button>
-           </a></li>
-        
-      </ul>
+            <li><a href="{{ url('/') }}" class="nav-link">All Contests</a></li>
+            <li><a href="{{ url('/hackerearth') }}" class="nav-link">Hacker Earth</a></li>
+            <li><a href="{{ url('/codeforces') }}" class="nav-link">CodeForces</a></li>
+
+            <li><a href="{{ url('/topcoder') }}" class="nav-link">TopCoder</a></li>
+            <li><a href="{{ url('/atcoder') }}" class="nav-link">AtCoder</a></li>
+            <li><a href="{{ url('/codechef') }}" class="nav-link">CodeChef</a></li>
+            <li><a href="{{ url('/hackerrank') }}" class="nav-link">HackerRank</a></li>
+            <li><a href="{{ url('/kickstart') }}" class="nav-link">Kick Start</a></li>
+            <li><a href="{{ url('/leetcode') }}" class="nav-link">LeetCode</a></li>
+            <li><a href="{{ url('/sites') }}" class="nav-link">Contest Sites</a></li>
+            <li><a target="_blank" href="https://www.facebook.com/collins.sharks.3/">
+                    <button class="btn btn-primary-solid btn-rounded"><i class="fa fa-facebook"
+                            style="font-size:28px;"></i></button>
+                </a></li>
+            <li><a target="_blank" href="https://github.com/rono516/">
+                    <button class="btn btn-primary-solid btn-rounded"><i class="fa fa-github"
+                            style="font-size:28px"></i></button>
+                </a></li>
+            <li><a href="https://twitter.com/collins_rono_" target="_blank">
+                    <button class="btn btn-primary-solid btn-rounded"><i class="fa fa-twitter"
+                            style="font-size:28px"></i></button>
+                </a></li>
+
+        </ul>
 
     </div>
-    </div>
-  </div>
-    
-  </nav>
+
+
+</nav>
